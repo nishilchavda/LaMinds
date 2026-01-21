412601a4ef545abde0b52ae6228f38c6ef0e10eb
nishilchavda <nishilchavda@gmail.com>
2026-01-21 17:17:05 +0530

    Initial commit

---
+++ JS/operator.js
@@ -0,0 +1,20 @@
+// operators
+// Arithmetic, comparioson, logical, assignment, unary, ternary
+// Arithmetic:
+// +, -, /, *, **, %
+// + --> Add and concatenation
+// ex. 1+2 --> Add
+// "user" + "name" --> concat
+// 5 + "5" --> concat, 5 + 5 --> Add
+// -
+// ex. 2 - 1
+// /,*
+// ex. 240/2, 25*2
+// % (modules) (sheshfal) (reminder)
+// ex. 12%4 --> 0
+// ex. 9%2 --> 1
+// ** (square)(exposition)
+// ex. 2**3 --> 8
+// ex. 2**3 --> 8
+
+
