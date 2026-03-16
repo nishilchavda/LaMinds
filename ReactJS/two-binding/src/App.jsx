904d4ca2628f9c88e57ea8da8195e84bbb4137ad
nishilchavda <nishilchavda@gmail.com>
2026-03-16 17:37:04 +0530

    two-binding

---
+++ ReactJS/two-binding/src/App.jsx
@@ -0,0 +1,13 @@
+import Login from './Components/Login'
+
+const App = () => {
+  return (
+    <>
+      {/* Two Way binding: manage form filed using usestate  */}
+
+      <Login /> 
+    </>
+  )
+}
+
+export default App
