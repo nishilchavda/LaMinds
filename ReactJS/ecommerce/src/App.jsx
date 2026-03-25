638d9324198501f60c4153b3f67973bd1aa51184
nishilchavda <nishilchavda@gmail.com>
2026-03-25 18:01:59 +0530

    Reupload useeffect

--- ReactJS/ecommerce/src/App.jsx
+++ ReactJS/ecommerce/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Product from "./Pages/Product";
+import Navbar from "./Components/Navbar";
 
 const App = () => {
   return (
     <>
+      <Navbar/>
       <Routes>
         <Route>
           <Route path="/" element={<Home />} />
